eb88413e177b108443107d3e04d3625357332b77
saisindhu96 <saisindhuraju@gmail.com>
2021-07-02 16:57:47 +0530

    added

--- examples/ec2instance-with-sg/ec2.tf
+++ examples/ec2instance-with-sg/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "sample" {
-  ami                   = "ami-059e6ca6474628ef0"
-  instance_type         = "t2.micro"
+  ami                    = "ami-059e6ca6474628ef0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
 }
 
@@ -10,4 +11,31 @@ provider "aws" {
 
 output "public_ip" {
   value = aws_instance.sample.public_ip
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
