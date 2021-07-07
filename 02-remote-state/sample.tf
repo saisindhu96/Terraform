8b278c04070682aa7e81286f59714e575457c8ea
saisindhu96 <saisindhuraju@gmail.com>
2021-07-07 10:24:44 +0530

    added

--- 02-remote-state/sample.tf
+++ 02-remote-state/sample.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "sample" {
   ami                    = "ami-059e6ca6474628ef0"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
 }
 
 terraform {
