3b12ffc46fd2695c5c2d9b914a3087b15cc60862
saisindhu96 <saisindhuraju@gmail.com>
2021-07-07 18:34:21 +0530

    added

--- 03-modules/ec2/ec2.tf
+++ 03-modules/ec2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "sample" {
   ami                    = "ami-059e6ca6474628ef0"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [var.sg_id]
+  vpc_security_group_ids = [var.sg_ig]
 
 }
 
