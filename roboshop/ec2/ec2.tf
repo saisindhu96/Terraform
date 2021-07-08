93fe07162cbd4839851f7fcf188640a64ed9b292
saisindhu96 <saisindhuraju@gmail.com>
2021-07-08 21:12:45 +0530

    added

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -14,9 +14,9 @@ variable "COMPONENTS" {}
 
 
 resource "null_resource" "wait" {
-  depends_on = [aws_spot_instance_request.cheap_worker]
-  triggers = {
-    abc = timestamp()
+  depends_on             = [aws_spot_instance_request.cheap_worker]
+  triggers               = {
+    abc                  = timestamp()
   }
 
   provisioner "local-exec" {
@@ -25,11 +25,19 @@ resource "null_resource" "wait" {
 }
 
 resource "aws_ec2_tag" "spot" {
-  depends_on          = [null_resource.wait]
+  depends_on             = [null_resource.wait]
   count                  = length(var.COMPONENTS)
   resource_id            = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
   key                    = "Name"
   value                  = element(var.COMPONENTS, count.index)
+}
+
+resource "aws_ec2_tag" "monitor" {
+  depends_on             = [null_resource.wait]
+  count                  = length(var.COMPONENTS)
+  resource_id            = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+  key                    = "Monitor"
+  value                  = "yes"
 }
 
 resource "aws_route53_record" "dns" {
