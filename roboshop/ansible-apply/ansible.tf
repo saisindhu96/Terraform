9b374a117269c56cdaa3e74ef4af808f809e34ff
saisindhu96 <saisindhuraju@gmail.com>
2021-07-08 17:30:49 +0530

    added

--- roboshop/ansible-apply/ansible.tf
+++ roboshop/ansible-apply/ansible.tf
@@ -13,7 +13,7 @@ resource "null_resource" "ansible-apply" {
       "sudo yum remove ansible -y",
       "sudo rm -rf /usr/lib/python2.7/site-packages.ansible*",
       "sudo pip install ansible -y",
-      "ansible-pull -i localhost, -U https://github.com/saisindhu96/ansible.git roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
+      "ansible-pull -U https://github.com/saisindhu96/ansible.git roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
     ]
 
   }
