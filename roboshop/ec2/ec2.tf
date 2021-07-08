035efa3950827273689e822db01c3715b60c86f3
saisindhu96 <saisindhuraju@gmail.com>
2021-07-08 16:57:56 +0530

    added

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_spot_instance_request" "cheap_worker" {
-  count               = length(var.COMPONENTS)
-  ami                 = "ami-059e6ca6474628ef0"
-  spot_price          = "0.0031"
-  instance_type       = "t3.micro"
+  count                  = length(var.COMPONENTS)
+  ami                    = "ami-059e6ca6474628ef0"
+  spot_price             = "0.0031"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-02aea963894a03e73"]
-  tags                = {
-    Name              = element(var.COMPONENTS, count.index)
+  tags                   = {
+    Name                 = element(var.COMPONENTS, count.index)
   }
 }
 
@@ -13,22 +13,22 @@ variable "COMPONENTS" {}
 
 
 resource "time_sleep" "wait" {
-  depends_on          = [aws_spot_instance_request.cheap_worker]
-  create_duration     = "120s"
+  depends_on             = [aws_spot_instance_request.cheap_worker]
+  create_duration        = "120s"
   }
 
 resource "aws_ec2_tag" "spot" {
-  count               = length(var.COMPONENTS)
-  resource_id         = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
-  key                 = "Name"
-  value               = element(var.COMPONENTS, count.index)
+  count                  = length(var.COMPONENTS)
+  resource_id            = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+  key                    = "Name"
+  value                  = element(var.COMPONENTS, count.index)
 }
 
 resource "aws_route53_record" "dns" {
-  count               = length(var.COMPONENTS)
-  zone_id             = "Z0724247R4KE3AIA5B4P"
-  name                = "${element(var.COMPONENTS, count.index)}.roboshop.internal"
-  type                = "A"
-  ttl                 = "300"
-  records             = [element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)]
+  count                  = length(var.COMPONENTS)
+  zone_id                = "Z0724247R4KE3AIA5B4P"
+  name                   = "${element(var.COMPONENTS, count.index)}.roboshop.internal"
+  type                   = "A"
+  ttl                    = "300"
+  records                = [element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)]
 }
