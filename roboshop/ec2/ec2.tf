defad1378d73a97257bcb9483386fe17a59b90aa
saisindhu96 <saisindhuraju@gmail.com>
2021-07-08 17:19:14 +0530

    added

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -4,6 +4,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   spot_price             = "0.0031"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-02aea963894a03e73"]
+
   tags                   = {
     Name                 = element(var.COMPONENTS, count.index)
   }
